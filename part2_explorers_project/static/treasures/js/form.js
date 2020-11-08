d4b63d6f0e6c34a635aa22ed5db7241e09fce724
datalowe <datalowe@posteo.de>
2020-11-08 07:28:00 +0100

    (part2) make form.js grab and summarize 'meta' form data

--- part2_explorers_project/static/treasures/js/form.js
+++ part2_explorers_project/static/treasures/js/form.js
@@ -1,2 +1,15 @@
-const addFormBtn = document.getElementById('add-form-btn');
-alert(addFormBtn.innerHTML);
+// const addFormBtn = document.getElementById('add-form-btn');
+// alert(addFormBtn.innerHTML);
+
+const totalFormsInput = document.getElementsByName('form-TOTAL_FORMS')[0];
+const initFormsInput = document.getElementsByName('form-INITIAL_FORMS')[0];
+const minFormsInput = document.getElementsByName('form-MIN_NUM_FORMS')[0];
+const maxFormsInput = document.getElementsByName('form-MAX_NUM_FORMS')[0];
+
+const statusMsg = `
+    There are currently ${totalFormsInput.value} forms in use, excluding
+    the 'meta' form. There were initially ${initFormsInput.value} forms.
+    The minimum number of forms allowed is ${minFormsInput.value}, and the
+    maximum is ${maxFormsInput.value}.
+`
+alert(statusMsg);
