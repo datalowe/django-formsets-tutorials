90e25b01d01b53b215d390f28d1be5af7bfe3c73
datalowe <datalowe@posteo.de>
2020-11-08 07:01:13 +0100

    (part2) add 'static'/'static_root' subdirectories of root directory. add corresponding specifications for STATIC_ROOT and STATICFILES_DIRS in settings.py

--- part2_explorers_project/explorer_project/settings.py
+++ part2_explorers_project/explorer_project/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_root'
+STATICFILES_DIRS = [BASE_DIR / 'static']
